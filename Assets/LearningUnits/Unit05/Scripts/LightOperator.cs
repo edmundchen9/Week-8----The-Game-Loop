1fd693eb71d87144c2c18b5c1a9b8a6ec81bb8d6
EdChen9 <ec3ukf@virginia.edu>
2022-10-14 12:11:24 -0400

    all units completed

--- Assets/LearningUnits/Unit05/Scripts/LightOperator.cs
+++ Assets/LearningUnits/Unit05/Scripts/LightOperator.cs
@@ -4,53 +4,53 @@ using UnityEngine;
 
 public class LightOperator : MonoBehaviour
 {
-//     [SerializeField] GameObject redCube, blueCube;
-//     [SerializeField] Light pointLight;
+    [SerializeField] GameObject redCube, blueCube;
+    [SerializeField] Light pointLight;
 
-//     bool isRedCubeDetected, isBlueCubeDeteced;
+    bool isRedCubeDetected, isBlueCubeDetected;
 
-//     // DIRECTIONS: We want the light to turn on when ONLY the red cube is in the trigger zone
-//     // You only need to make additions to the code in the Update() method;
+    // DIRECTIONS: We want the light to turn on when ONLY the red cube is in the trigger zone
+    // You only need to make additions to the code in the Update() method;
 
-//     // Start is called before the first frame update
-//     void Start()
-//     {
+    // Start is called before the first frame update
+    void Start()
+    {
         
-//     }
+    }
 
-//     // Update is called once per frame
-//     void Update()
-//     {
-//         // if...
-//         //pointLight.enabled = true;
+    // Update is called once per frame
+    void Update()
+    {
+        if (isRedCubeDetected && !isBlueCubeDetected){
+        pointLight.enabled = true;
+        }
+        else
+        pointLight.enabled = false;
+    }
 
-//         // else
-//         //pointLight.enabled = false;
-//     }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == blueCube)
+        {
+            isBlueCubeDetected = true;
+        }
 
-//     private void OnTriggerEnter(Collider other)
-//     {
-//         if (other.gameObject == blueCube)
-//         {
-//             isBlueCubeDeteced = true;
-//         }
+        if (other.gameObject == redCube)
+        {
+            isRedCubeDetected = true;
+        }
+    }
 
-//         if (other.gameObject == redCube)
-//         {
-//             isRedCubeDetected = true;
-//         }
-//     }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject == blueCube)
+        {
+            isBlueCubeDetected = false;
+        }
 
-//     private void OnTriggerExit(Collider other)
-//     {
-//         if (other.gameObject == blueCube)
-//         {
-//             isBlueCubeDeteced = false;
-//         }
-
-//         if (other.gameObject == redCube)
-//         {
-//             isRedCubeDetected = false;
-//         }
-//     }
+        if (other.gameObject == redCube)
+        {
+            isRedCubeDetected = false;
+        }
+    }
 }
